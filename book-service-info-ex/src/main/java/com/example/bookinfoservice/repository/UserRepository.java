fcdb0cce7fcd2449dcfb92ad144ca43fd0df9a14
WitseM <witse.meeussen@telenet.be>
2022-01-20 15:48:18 +0100

    shite

--- book-service-info-ex/src/main/java/com/example/bookinfoservice/repository/UserRepository.java
+++ book-service-info-ex/src/main/java/com/example/bookinfoservice/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.example.bookinfoservice.repository;
-import com.example.bookinfoservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
